52fcb47b526a366fcfe4b7d0392a35e4384faa24
Fernando Couto <fernando.avcouto@gmail.com>
2025-01-31 11:49:58 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Fernando Santos
+    <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#2e026d] to-[#15162c] text-white">
+      <span className="text-red-200">Fernando Santos</span>
     </main>
   );
 }
